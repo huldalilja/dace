ad96b7f61cb510c9ac49c1ef651fb4155b96b049
Hulda Lilja Hannesdóttir <hulda@huldalilja.com>
2022-12-09 16:29:47 +0100

    making alpha, beta, M, N and K parameters to function in tasklet and all the way to kernel

--- dace/libraries/blas/nodes/gemm.py
+++ dace/libraries/blas/nodes/gemm.py
@@ -477,10 +477,10 @@ class ExpandGemmTensorCore2(ExpandTransformation):
             opt['ldc'] = 'M'
             opt['major_order'] = 'col'
 
-        #######################
+        #######################################################################
         code_io_stream = CodeIOStream()
         host_code_io_stream = CodeIOStream()
-        host_code_io_stream.write("""DACE_EXPORTED void calling_gemm(dace::float16* a, dace::float16* b, float* c);
+        host_code_io_stream.write("""DACE_EXPORTED void calling_gemm(dace::float16* a, dace::float16* b, float* c, float alpha, float beta, int M, int N, int K);
         """)
         sdfg.append_global_code(host_code_io_stream.getvalue())
 
@@ -510,14 +510,6 @@ class ExpandGemmTensorCore2(ExpandTransformation):
 
 // GEMM configuration.
 
-#define M_TILES 64
-#define N_TILES 64
-#define K_TILES 64
-
-#define M_GLOBAL (WMMA_M * M_TILES)
-#define N_GLOBAL (WMMA_N * N_TILES)
-#define K_GLOBAL (WMMA_K * K_TILES)
-
 #define C_LAYOUT wmma::mem_row_major
 
 // Implementation constants.
@@ -526,14 +518,6 @@ class ExpandGemmTensorCore2(ExpandTransformation):
 #define THREADS_PER_BLOCK (WARP_SIZE * WARPS_PER_BLOCK)
 
 #if SHARED_MEMORY_LIMIT_64K
-// With only 64 Kb shared memory available, we can fit two 8-tile chunks of
-// the A and B matrix data, that are 16 * 16 * 8 * 8 * 2 = 32 Kb each
-// (i.e. two 8x8 arrays of tiles of 16x16 half-typed elements per CTA).
-// But we cannot account the 8 Kb total skew overhead, without which the
-// performance would be severely impacted. So we choose to reduce the chunk size
-// in half, i.e. the amount of A and B matrix data we cache in shared memory.
-// Accordingly, this doubles the number of outer iterations across the global WMMA_K
-// dimension, which only slightly impacts the performance.
 #define CHUNK_K 4
 #else
 #define CHUNK_K 8
@@ -553,24 +537,9 @@ class ExpandGemmTensorCore2(ExpandTransformation):
 #define BLOCK_ROW_TILES (WARP_ROW_TILES * BLOCK_ROW_WARPS)
 #define BLOCK_COL_TILES (WARP_COL_TILES * BLOCK_COL_WARPS)
 
-#define GLOBAL_MEM_STRIDE N_GLOBAL
-
 #define SHMEM_STRIDE (WMMA_N * BLOCK_ROW_TILES)
 #define SHMEM_OFFSET (WMMA_N * WARP_ROW_TILES)
 
-// The macro below is used to shift rows of the A matrix and columns of the B
-// matrix in shared memory to minimize possible bank conflicts. Before
-// performing the nvcuda::wmma::mma_sync operation, the warp must load the
-// matrix data using the nvcuda::wmma::load_matrix_sync operation. Although the
-// memory access pattern is not specified for that function, each lane in the
-// warp can read one or multiple matrix elements from different matrix rows or
-// columns. For shared memory, such access can result in bank conflicts if
-// different rows / columns of the matrix map to the same bank. By shifting each
-// row and column by a few bytes, we make sure that they map to different banks,
-// thus reducing the number of possible bank conflicts. The number of 8 two-byte
-// "half" elements is chosen as the minimum possible shift because we must keep
-// each row and column 128-bit aligned, as required by
-// nvcuda::wmma::load_matrix_sync.
 #define SKEW_HALF 8
 
 #define checkKernelErrors(expr)                             \\
@@ -589,8 +558,14 @@ using namespace nvcuda;
 
 
 __global__ void compute_gemm(const half *A, const half *B, float *C,
-                             float alpha, float beta) {
+                             float alpha, float beta, int M, int N, int K) {
   extern __shared__ half shmem[][CHUNK_K * WMMA_K + SKEW_HALF];
+
+  const unsigned int M_TILES = M / 16;
+  const unsigned int N_TILES = N / 16;
+  const unsigned int K_TILES = K / 16;
+
+  const unsigned int GLOBAL_MEM_STRIDE = N;
 
   // Warp and lane identification.
   const unsigned int warpId = threadIdx.x / WARP_SIZE;
@@ -647,7 +622,7 @@ __global__ void compute_gemm(const half *A, const half *B, float *C,
     __syncthreads();
 
     // These fragments will accumulate the result of A and B matrix fragment
-    // multiplications along the K_GLOBAL dimension.
+    // multiplications along the K dimension.
     wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, float> c[WARP_COL_TILES]
                                                        [WARP_ROW_TILES];
 
@@ -679,10 +654,10 @@ __global__ void compute_gemm(const half *A, const half *B, float *C,
 
     // Select what warp copies what matrix to shared memory.
     // Warps 0-3 copy the A matrix, warps 4-7 copy the B matrix.
-    const half *warp_ptr = (warpId < 4) ? (&A[block_tile_i * WMMA_M * K_GLOBAL] +
-                                           WMMA_M * K_GLOBAL * (warpId % 4) * 2)
-                                        : (&B[block_tile_j * WMMA_N * K_GLOBAL] +
-                                           WMMA_N * K_GLOBAL * (warpId % 4) * 2);
+    const half *warp_ptr = (warpId < 4) ? (&A[block_tile_i * WMMA_M * K] +
+                                           WMMA_M * K * (warpId % 4) * 2)
+                                        : (&B[block_tile_j * WMMA_N * K] +
+                                           WMMA_N * K * (warpId % 4) * 2);
 
     // Go through the global WMMA_K dimension by a fixed step at a time.
 #pragma unroll
@@ -698,7 +673,7 @@ __global__ void compute_gemm(const half *A, const half *B, float *C,
       // First half of the warp copies the first row / column of the matrix,
       // the second half of the warp copies the next.
       int4 *lane_ptr = (int4 *)(warp_ptr + tile_k * WMMA_K +
-                                (laneId / CHUNK_COPY_LINE_LANES) * K_GLOBAL) +
+                                (laneId / CHUNK_COPY_LINE_LANES) * K) +
                        (laneId % CHUNK_COPY_LINE_LANES);
 
       // Shift the second half of the warp to the next row / column in the
@@ -714,7 +689,7 @@ __global__ void compute_gemm(const half *A, const half *B, float *C,
 
         // Advance the global memory pointer and the shared memory index.
         lane_ptr =
-            (int4 *)((half *)lane_ptr + K_GLOBAL * CHUNK_COPY_LINES_PER_WARP);
+            (int4 *)((half *)lane_ptr + K * CHUNK_COPY_LINES_PER_WARP);
         shmem_idx += CHUNK_COPY_LINES_PER_WARP;
       }
 
@@ -789,8 +764,8 @@ __global__ void compute_gemm(const half *A, const half *B, float *C,
   }
 }
 
-DACE_EXPORTED void calling_gemm(dace::float16* a, dace::float16* b, float* c);
-void calling_gemm(dace::float16* a, dace::float16* b, float* c){
+DACE_EXPORTED void calling_gemm(dace::float16* a, dace::float16* b, float* c, float alpha, float beta, int M, int N, int K);
+void calling_gemm(dace::float16* a, dace::float16* b, float* c, float alpha, float beta, int M, int N, int K){
     // int dev = findCudaDevice(0, NULL);
 
     // cudaDeviceProp deviceProp;
@@ -811,9 +786,6 @@ void calling_gemm(dace::float16* a, dace::float16* b, float* c){
 
   // printf("Required shared memory size: %lu Kb\\n", SHMEM_SZ / 1024UL);
 
-  const float alpha = 1.1f;
-  const float beta = 1.2f;
-
   // If enough shared memory available on the GPU use high performant kernel
   //if (deviceProp.sharedMemPerMultiprocessor >= SHMEM_SZ) {
     // printf("Computing... using high performance kernel compute_gemm \\n");
@@ -822,7 +794,7 @@ void calling_gemm(dace::float16* a, dace::float16* b, float* c){
         compute_gemm, cudaFuncAttributeMaxDynamicSharedMemorySize, SHMEM_SZ));
     checkKernelErrors(
         (compute_gemm<<<68, THREADS_PER_BLOCK,
-                        SHMEM_SZ>>>(a, b, c, alpha, beta)));
+                        SHMEM_SZ>>>(a, b, c, alpha, beta, M, N, K)));
     //}
 }
 """
@@ -830,7 +802,7 @@ void calling_gemm(dace::float16* a, dace::float16* b, float* c){
         ###############################################################
 
         sdfg.append_global_code(code_io_stream.getvalue(), 'cuda')
-        code = """calling_gemm(_a, _b, _c);"""
+        code = """calling_gemm(_a, _b, _c, {alpha}, {beta}, {M}, {N}, {K});""".format_map(opt)
         tasklet = dace.sdfg.nodes.Tasklet(
             node.name,
             node.in_connectors,
